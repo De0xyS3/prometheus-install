ffd938151338e76d4c4d836ceacde2da3c2cee27
panconpalta <87393512+De0xyS3@users.noreply.github.com>
2024-01-13 10:26:31 -0500

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Download grafana
-wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.6.3_amd64.deb
 
-# Install grafana
-sudo apt-get install -y adduser libfontconfig
-sudo dpkg -i grafana_4.6.3_amd64.deb
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_10.0.0_amd64.deb
+sudo dpkg -i grafana-enterprise_10.0.0_amd64.deb
+
+
 
 # systemd
 sudo systemctl daemon-reload
@@ -13,4 +14,4 @@ sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 
 # Installation cleanup
-rm grafana_4.6.3_amd64.deb
+
